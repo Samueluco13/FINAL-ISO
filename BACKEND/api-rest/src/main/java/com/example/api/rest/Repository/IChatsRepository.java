0d34779f7a61dd73f718c7b3407c3ccc201e54bb
Dieguito <diego_a.hernandez@uao.edu.co>
2025-05-20 16:35:02 -0500

    Cambio

--- BACKEND/api-rest/src/main/java/com/example/api/rest/Repository/IChatsRepository.java
+++ BACKEND/api-rest/src/main/java/com/example/api/rest/Repository/IChatsRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.api.rest.Model.ChatsModel;
 
 public interface IChatsRepository extends MongoRepository<ChatsModel, ObjectId>{
-    List<ChatsModel> findByNombreUsuarioDestinatarioAndNombreUsuarioRemitenteOrderByFechaAscHoraAsc(String destinatario, String remitente);
     List<ChatsModel> findByNombreUsuarioDestinatario(String nombreUsuarioDestinatario);
     Optional<ChatsModel> findByNombreUsuarioDestinatarioAndNombreUsuarioRemitente(String nombreUsuarioDestinatario, String nombreUsuarioRemitente);
 
