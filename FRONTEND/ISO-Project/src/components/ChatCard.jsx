deefc989b1254a12c1005ac67eb1129e7a5ca87c
Samuel <sepulvedacsamuel09@gmail.com>
2025-05-25 16:55:24 -0500

    Visto en mensajes

--- FRONTEND/ISO-Project/src/components/ChatCard.jsx
+++ FRONTEND/ISO-Project/src/components/ChatCard.jsx
@@ -8,7 +8,16 @@ export const ChatCard = ({chat, onChat, onDelete}) => {
                 <h4 onClick={() => onChat(chat)} className='chat-card-title' >{chat.avisoNombre}</h4>
                 <p><strong>{chat.mensajes.at(-1)?.nombreUsuario || "Sin mensajes"}</strong>: {chat.mensajes.at(-1)?.contenido || "Aún no hay mensajes."}</p>
                 {chat.mensajes.at(-1) && (
-                    <small>{new Date(chat.mensajes.at(-1)?.fecha).toLocaleString()}</small>
+                    <small>
+                        {new Date(chat.mensajes.at(-1)?.fecha).toLocaleString('es-CO', {
+                            hour12: false,
+                            year: 'numeric',
+                            month: '2-digit',
+                            day: '2-digit',
+                            hour: '2-digit',
+                            minute: '2-digit'
+                        })}
+                    </small>
                 )}
             </div>
             <div className="chat-actions">
